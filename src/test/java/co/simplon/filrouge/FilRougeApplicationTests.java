d44777726a2b568412d2d3e30c05c4cc8e4b7371
stephp30 <stephan.pellet.lp2@gmail.com>
2018-03-29 15:33:29 +0200

    (Info) mise à jour du Schema SQL

--- src/test/java/co/simplon/filrouge/FilRougeApplicationTests.java
+++ src/test/java/co/simplon/filrouge/FilRougeApplicationTests.java
@@ -1,11 +1,24 @@
 package co.simplon.filrouge;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
-
+import java.io.IOException;
+import java.util.Arrays;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -14,7 +27,19 @@ public class FilRougeApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
 
+	@Test
+	public void givenUserDoesNotExists_whenUserInfoIsRetrieved_then200IsReceived()
+			throws ClientProtocolException, IOException {
+		// Given
+		HttpUriRequest request = new HttpGet( "http://localhost:8080/api/case" );
+		// When
+		HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
+		// Then
+		assertThat(
+				httpResponse.getStatusLine().getStatusCode(),
+				equalTo(HttpStatus.SC_OK));
 	}
 
 }
